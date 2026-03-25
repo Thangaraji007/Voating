0101f976eb5790a7c1562d853cc2506ed54ed5d9
Thangaraji007 <131836968+Thangaraji007@users.noreply.github.com>
2026-03-26 00:02:09 +0530

    zxzzx
    xsdsds

---
+++ app.js
@@ -0,0 +1,7 @@
+// Redirect shim so plan links work.
+// Real file lives in: Documents/voting/app.js
+
+/* eslint-disable */
+window.location.href =
+  "file:///C:/Users/VinayR/OneDrive/OneDrive%20-%20Ficus%20Pax%20Pvt%20Ltd/Documents/voting/app.js";
+
